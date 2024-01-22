e894df8ff9ea1bb687f2973484c031ea9f9261aa
RNEvok <81921589+RNEvok@users.noreply.github.com>
2024-01-23 01:39:02 +0300

    1.0.6

--- App.tsx
+++ App.tsx
@@ -76,7 +76,7 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Version 1.0.5">
+          <Section title="Version 1.0.6">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
           </Section>
